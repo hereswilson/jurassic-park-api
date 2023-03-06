7662dbb4f388b0335f034aa9cd817693ae321ffc
Wilson <wheres67@gmail.com>
2023-03-05 21:48:48 -0500

    Remove business logic from cage model

--- models/cage.go
+++ models/cage.go
@@ -21,21 +21,3 @@ var (
 	ErrDinosaurInDifferentCage = errors.New("dinosaur cannot be in this cages")
 	ErrCagePoweredOn           = errors.New("cage powered on")
 )
-
-func (cage *Cage) BeforeDelete(tx *gorm.DB) (err error) {
-	// Check if cage is powered on before allowing deletion
-	if cage.PowerStatus != "DOWN" {
-		return errors.New("cannot delete a cage that is not powered down")
-	}
-
-	return nil
-}
-
-func (cage *Cage) BeforeUpdate(tx *gorm.DB) (err error) {
-	// Check if cage is powered down before allowing power status update
-	if tx.Statement.Changed("PowerStatus") && cage.PowerStatus != "DOWN" {
-		return errors.New("cannot power off a cage that contains dinosaurs")
-	}
-
-	return nil
-}
