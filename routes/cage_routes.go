655477d7d220e43619da69dc4352c019bf4cd443
Wilson <wheres67@gmail.com>
2023-03-05 19:39:51 -0500

    fix initialization

--- routes/cage_routes.go
+++ routes/cage_routes.go
@@ -2,11 +2,18 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+
 	"github.com/hereswilson/jurassic-park-api/controllers"
+	"github.com/hereswilson/jurassic-park-api/services"
+	"github.com/hereswilson/jurrasic-park-api/repositories"
+
+	"gorm.io/gorm"
 )
 
-func cageRoutes(superRoute *gin.RouterGroup) {
-	cageController := &controllers.CageController{}
+func cageRoutes(superRoute *gin.RouterGroup, db *gorm.DB) {
+	cageRepo := repositories.NewCageRepository(db)
+	cageService := services.NewCageService(cageRepo)
+	cageController := controllers.NewCageController(cageService)
 	cageRouter := superRoute.Group("/cages")
 	{
 		cageRouter.GET("/", cageController.GetCages)
--- routes/index.go
+++ routes/index.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func AddRoutes(superRoute *gin.RouterGroup) {
-	cageRoutes(superRoute)
-	dinosaurRoutes(superRoute)
-	speciesRoutes(superRoute)
+func AddRoutes(superRoute *gin.RouterGroup, db *gorm.DB) {
+	cageRoutes(superRoute, db)
+	dinosaurRoutes(superRoute, db)
+	speciesRoutes(superRoute, db)
 }
